6154a2c2b1af52db221af2006e44ae0bd4b520af
bilalameersl <bilal.ameer@systemsltd.com>
2020-06-21 20:06:38 +0500

    Axios interceptors and post call to save track

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native'
 
-export default axios.create({
+const instance = axios.create({
     baseURL: 'http://cd12a0bf0578.ngrok.io'
 });
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (err) => {
+        return Promise.resolve(err);
+    }
+)
+
+export default instance;
