9bbf5f569b4bc0a3a29577f5f87be431f44f3f23
bilalameersl <bilal.ameer@systemsltd.com>
2020-06-22 05:25:44 +0500

    Track list and its details

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { Context as TrackContext } from '../context/TrackContext';
+import MapView, { Polyline } from 'react-native-maps';
 
-const TrackDetailScreen = () => {
+const TrackDetailScreen = ({ navigation }) => {
+
+    const _id = navigation.getParam('_id');
+    const { state } = useContext(TrackContext);
+    const track = state.find(t => t._id === _id);
+    const initialCoordinates = track.locations[0].coords;
     return (
         <View>
-            <Text>TrackDetailScreen</Text>
+            <Text>{track.name}</Text>
+            <MapView style={styles.map}
+                initialRegion={{
+                    ...initialCoordinates,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01
+                }}>
+                <Polyline
+                    coordinates={track.locations.map(location => location.coords)}
+                />
+            </MapView>
         </View>
     );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    map: {
+        height: 300
+    }
+});
 export default TrackDetailScreen;
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import { View, Text, StyleSheet,Button } from 'react-native';
+import React, { useContext } from 'react';
+import { Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+import { Context as TrackContext } from '../context/TrackContext';
+import { ListItem } from 'react-native-elements';
 
-const TrackListScreen = ({navigation}) => {
+const TrackListScreen = ({ navigation }) => {
+    const { state, fetchTracks } = useContext(TrackContext);
     return (
-        <View>
-            <Text>TrackListScreen</Text>
-            <Button title='Goto TrackDetail' onPress={()=>navigation.navigate('TrackDetail')} />
-        </View>
+        <>
+            <NavigationEvents onWillFocus={fetchTracks} />
+            <FlatList
+                data={state}
+                keyExtractor={item => item._id}
+                renderItem={({ item }) => {
+                    return <TouchableOpacity onPress={() => {
+                        navigation.navigate('TrackDetail', { _id: item._id })
+                    }}>
+                        <ListItem chevron title={item.name} />
+                    </TouchableOpacity>
+                }}
+            />
+        </>
     );
 };
 const styles = StyleSheet.create({});
